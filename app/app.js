29ef4c3013158e818c85278c90dc8f1f24953d5d
Christopher McLean <cmclean@unikey.com>
2017-02-12 08:12:47 -0500

    added base styling

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ require("./styles/main.scss");
 
 import angular from 'angular';
 import TreeFormComponent from './components/tree-form';
+import NavbarComponent from './components/navbar';
+import FooterComponent from './components/footer';
 
 angular.module('app', []);
-angular.module('app').component('treeForm', TreeFormComponent);
+angular.module('app').component('cfoTreeForm', TreeFormComponent);
+angular.module('app').component('cfoNavbar', NavbarComponent);
+angular.module('app').component('cfoFooter', FooterComponent);
---
+++ app/components/footer.js
@@ -0,0 +1,9 @@
+/**
+ * @module
+ * @description Component responsible for displaying the site's footer
+ */
+export default {
+
+  /** @type { String } */
+  templateUrl: 'app/components/footer.html',
+}
